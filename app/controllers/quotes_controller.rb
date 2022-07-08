eee512b045eb13e8476124f47debfab32ccbb8df
Orson <orson.t99@gmail.com>
2022-07-08 12:08:00 +0800

    Apply Turbo stream on quote deletion

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -35,7 +35,11 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: 'Quote was succesfully destroyed.'
+    
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: 'Quote was sucesfully destroyed.' }
+      format.turbo_stream
+    end
   end
 
   private
