5f308c83494e48ee58859bb6c4cc878c28a7bb1b
Orson <orson.t99@gmail.com>
2022-07-08 11:15:02 +0800

    Remove comments in quote test

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -7,23 +7,15 @@ class QuotesTest < ApplicationSystemTestCase
   end
 
   test 'Creating a new quote' do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text 'Quotes'
     visit quotes_path
     assert_selector 'h1', text: 'Quotes'
 
-    # When we click on the link with the text 'New quote'
-    # we expect to land on a page with the title 'New quote'
     click_on 'New quote'
-    assert_selector 'h1', text: 'New quote'
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
     fill_in 'Name', with: 'Capybara quote'
-    click_on 'Create Quote'
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
+    assert_selector 'h1', text: 'Quotes'
+    click_on 'Create quote'
+
     assert_selector 'h1', text: 'Quotes'
     assert_text 'Capybara quote'
   end
@@ -40,10 +32,10 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Quotes'
 
     click_on 'Edit', match: :first
-    assert_selector 'h1', text: 'Edit quote'
-
     fill_in 'Name', with: 'Updated quote'
-    click_on 'Update Quote'
+
+    assert_selector 'h1', text: 'Quotes'
+    click_on 'Update quote'
 
     assert_selector 'h1', text: 'Quotes'
     assert_text 'Updated quote'
